df1ddf55a1d117a68e5fe4af6036cd40481a6c31
ronbarrantes <ronbarrantes@gmail.com>
2023-08-11 15:34:07 -0700

    Update titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,13 @@ export default function Home() {
     <div className="flex flex-col w-screen h-screen p-5 gap-5">
       <header className="text-slate-300 flex flex-col text-sm justify-between">
         <h1 className="text-2xl italic text-emerald-200 mb-3">
-          Drag and Drop challenge
+          Drag and Drop Challenge
         </h1>
-        <p>Drag nodes onto the server</p>
-        <p>the server will detect which server the node was dragged into</p>
+        <p>Drag nodes to the servers</p>
+        <p>
+          When a node is dropped onto a server, the server will notify that said
+          node was dragged
+        </p>
       </header>
       <main className="flex flex-col items-center justify-between">
         <Board />
